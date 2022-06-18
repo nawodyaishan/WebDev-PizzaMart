627b56bef997eaa4ecba47115db5075ab72fd385
nawodyaishan <nawodyain@gmail.com>
2022-06-18 23:05:34 +0530

    Add I Action Result Method, Price Increments, Redirect to Checkout

--- PizzaMart/Pages/Forms/CustomPizza.cshtml.cs
+++ PizzaMart/Pages/Forms/CustomPizza.cshtml.cs
@@ -8,8 +8,23 @@ namespace PizzaMart.Pages.Forms;
 public class CustomPizza : PageModel
 {
     [BindProperty] public PizzasModel Pizza { get; private set; }
+    public float PizzaPrice { get; set; }
 
     public void OnGet()
     {
+    }
+
+    public IActionResult OnPost()
+    {
+        PizzaPrice = Pizza.BasePrice;
+
+        if (Pizza.TomatoesSauce) PizzaPrice++;
+        if (Pizza.Cheese) PizzaPrice++;
+        if (Pizza.PineApple) PizzaPrice += 10;
+        if (Pizza.Beef) PizzaPrice += 2;
+        if (Pizza.Mushrooms) PizzaPrice += 3;
+        if (Pizza.Tuna) PizzaPrice += 4;
+
+        return RedirectToPage("/Checkout/Checkout");
     }
 }
