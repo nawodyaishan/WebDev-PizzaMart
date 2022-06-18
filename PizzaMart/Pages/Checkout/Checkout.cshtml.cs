25773164cb3d64a24587e0761cabc39d4efa85ef
nawodyaishan <nawodyain@gmail.com>
2022-06-18 23:36:16 +0530

    Add Binding

--- PizzaMart/Pages/Checkout/Checkout.cshtml.cs
+++ PizzaMart/Pages/Checkout/Checkout.cshtml.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace PizzaMart.Pages.Checkout;
 
+[BindProperties]
 public class Checkout : PageModel
 {
     public string PizzaName { get; set; }
