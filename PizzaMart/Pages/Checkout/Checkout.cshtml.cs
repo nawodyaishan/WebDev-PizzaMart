895b74e91734575d852098ca32f20803371d1e98
nawodyaishan <nawodyain@gmail.com>
2022-06-19 00:01:15 +0530

    Add PizzaImageTitle, Checkout.cshtml

--- PizzaMart/Pages/Checkout/Checkout.cshtml.cs
+++ PizzaMart/Pages/Checkout/Checkout.cshtml.cs
@@ -3,14 +3,23 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace PizzaMart.Pages.Checkout;
 
-[BindProperties]
+[BindProperties(SupportsGet = true)]
 public class Checkout : PageModel
 {
     public string PizzaName { get; set; }
     public float PizzaPrice { get; set; }
-    public string PizzaImage { get; set; }
+    public string PizzaImageTitle { get; set; }
 
     public void OnGet()
     {
+        if (string.IsNullOrWhiteSpace(PizzaName))
+        {
+            PizzaName = "Custom";
+        }
+
+        if (string.IsNullOrWhiteSpace(PizzaImageTitle))
+        {
+            PizzaImageTitle = "wooden.png";
+        }
     }
 }
